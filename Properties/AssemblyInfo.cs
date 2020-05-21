7aeeee90fd63763f222cc930347a95abf8d948a2
Maksym <Maksym@DESKTOP-62A1UHE>
2020-05-21 15:14:23 +0300

    Maintenance

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StarCraftMapBrowser")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 Maksym Alokhin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
